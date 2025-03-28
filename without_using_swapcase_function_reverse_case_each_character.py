75075cf82a07dd00cd86d84a00d9b7c338fcff74
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 12:51:44 +0800

    initial idea on the pseudocode

--- without_using_swapcase_function_reverse_case_each_character.py
+++ without_using_swapcase_function_reverse_case_each_character.py
@@ -1,1 +1,9 @@
 #swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+#PROGRAM START:
+# DISPLAY "Enter a string to swap cases:"
+# GET input from user
+# swapped_output = CALL swap(user_input)
+# DISPLAY "Case-swapped result:"
+# DISPLAY swapped_output
+# PROGRAM END 
