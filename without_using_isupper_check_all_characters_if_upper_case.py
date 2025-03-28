8e0b81da2ee2d4cecff413c8eb7f1c15738f9c24
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 18:36:31 +0800

    added initial pseudo code

--- without_using_isupper_check_all_characters_if_upper_case.py
+++ without_using_isupper_check_all_characters_if_upper_case.py
@@ -1,1 +1,10 @@
 #isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+#FUNCTION is_all_uppercase(string):
+#IF string is empty:
+# RETURN False
+# FOR each character IN string:
+# IF character is alphabetic:
+# IF character's ASCII value is NOT between 65 ('A') and 90 ('Z'):
+# RETURN False
+# RETURN True  # All alphabetic chars are uppercase
