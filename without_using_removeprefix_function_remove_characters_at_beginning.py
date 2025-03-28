ad097db8262a4dff2c15259f9a53f5a8ebad97d4
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 13:24:49 +0800

    initial idea or pseudo code

--- without_using_removeprefix_function_remove_characters_at_beginning.py
+++ without_using_removeprefix_function_remove_characters_at_beginning.py
@@ -1,1 +1,10 @@
 #removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+#FUNCTION custom_removeprefix(s, prefix):
+# IF string 's' starts with 'prefix':
+# RETURN substring of 's' starting after 'prefix'
+# ELSE:
+# RETURN original string 's'
+
+
+
