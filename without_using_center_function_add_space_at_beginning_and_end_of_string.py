456c543de51e8d0e6458b8ce8342081746a9b436
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 19:51:48 +0800

    added pseudocode for the possible program flow

--- without_using_center_function_add_space_at_beginning_and_end_of_string.py
+++ without_using_center_function_add_space_at_beginning_and_end_of_string.py
@@ -1,1 +1,14 @@
 #center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# FUNCTION custom_center(string, width, fillchar)
+# padding ← MAX(0, width - LENGTH(string)) 
+# left_padding ← padding 
+# right_padding ← padding - left_padding   
+# RETURN (fillchar * left_padding) + string + (fillchar * right_padding)  
+
+# IF LENGTH(fill) = 0 THEN
+# fill ← ' '  
+# ELSE fill ← fill[0]  
+# ENDIF
+
+    
