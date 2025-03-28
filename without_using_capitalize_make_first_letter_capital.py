9a0b395dedeb2b75d14ad8a5cdbc85496baa8225
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 20:36:37 +0800

    added pseudocode for the function

--- without_using_capitalize_make_first_letter_capital.py
+++ without_using_capitalize_make_first_letter_capital.py
@@ -1,1 +1,9 @@
 # capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+#FUNCTION custom_capitalize(text):
+# IF text is empty:
+# RETURN text  # Handle empty string case
+# first_char = CONVERT text[0] TO UPPERCASE
+# remaining_chars = CONVERT text[1:] TO LOWERCASE
+# RETURN first_char + remaining_chars
+# END FUNCTION
