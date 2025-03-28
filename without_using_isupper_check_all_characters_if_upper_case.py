373e9d851c080ec3ff368cb9f2b0d3e21630255b
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 18:53:36 +0800

    used .isalpha to check if the characters in string are alphabetic letters, added a conditional check A = 65 Z = 90, if the Unicode code point of char falls inside the range of uppercase letters

--- without_using_isupper_check_all_characters_if_upper_case.py
+++ without_using_isupper_check_all_characters_if_upper_case.py
@@ -8,3 +8,9 @@
 # IF character's ASCII value is NOT between 65 ('A') and 90 ('Z'):
 # RETURN False
 # RETURN True  # All alphabetic chars are uppercase
+
+def is_all_uppercase(s):
+    for char in s:
+        if char.isalpha() and not (65 <= ord(char) <= 90):
+            return False
+        return bool(s) 
