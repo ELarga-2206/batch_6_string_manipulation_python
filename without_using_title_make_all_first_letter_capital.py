fd0b721421efecd5a47723f1e908f25f1c1d57d9
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 12:42:36 +0800

    rewritten the code for target function, tested and completed the code

--- without_using_title_make_all_first_letter_capital.py
+++ without_using_title_make_all_first_letter_capital.py
@@ -1,17 +1,10 @@
 #title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
-#PROGRAM START
-    # DISPLAY "Enter a sentence to convert to title case:"
-    # GET input from user
-    # output = CALL simple_title(user_input) 
-    # DISPLAY title_case_output
-
 def simple_title(s):
-    return ' '.join(w.capitalize() for w in s.split())
+    return ' '.join(word.capitalize() for word in s.split())
 
-titled_words = []
-    for word in s.split():
-        if word:
-            titled_word = word[0].upper() + word[1:].lower()
-            titled_words.append(titled_word)
-    return ' '.join(titled_words)
+user_input = input("Enter a sentence to convert to title case: ")
+
+title_case_output = simple_title(user_input)
+print("\nConverted to title case:")
+print(title_case_output)
