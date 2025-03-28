c38dbb77268f901f3bf0764e2769e56d8995f8dc
Elarga-2206 <earl.larga22@gmail.com>
2025-03-28 17:04:32 +0800

    removed pseudocode, fixed the issue on line 17 where i forgot the '=', added 'chr' and 'ord' function to know the numberical Id for character and who has them, fixed indention line 10

--- without_using_lower_function_convert_character_to_lower_case.py
+++ without_using_lower_function_convert_character_to_lower_case.py
@@ -1,24 +1,13 @@
 #lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
-#FUNCTION to_lowercase(s):
-    #CREATE empty result string
-    #FOR each character in input string s:
-        #GET ASCII value of character
-        #IF character is uppercase (A-Z):
-            #CONVERT to lowercase by adding 32 to ASCII value
-            #ADD converted character to result
-        #ELSE:
-            #ADD character to result unchanged
-    #RETURN result
-
 def to_lowercase(s):
     result = []
     for char in s:
-        if 65 < ord(char) < 90:
-            result.append((char) + 32)
+        if 65 <= ord(char) <= 90:
+            result.append(chr(ord(char) + 32))
         else:
             result.append(char)
-        return ''.join(result)
+    return ''.join(result)
     
 user_input = input("Enter a string to convert to lowercase: ")
 
