ba7040cc9b4f836612a17604662bbe35b0d7686f
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 13:14:26 +0800

    added pseudocode for guide

--- batch_7_without_using_islower_check_if_string_on_lower_case.py
+++ batch_7_without_using_islower_check_if_string_on_lower_case.py
@@ -1,1 +1,8 @@
 #  islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+#F0UNCTION is_all_lower(text):
+# IF text is empty: RETURN False
+# 
+# FOR each character IN text:
+# IF character is a letter AND NOT lowercase:
+# RETURN False
