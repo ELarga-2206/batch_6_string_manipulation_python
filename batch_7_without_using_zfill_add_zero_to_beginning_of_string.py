3a006be2e09c1ef6b0637fe95f7a4108ff86038e
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 17:03:46 +0800

    added user input and application of the custom zfill

--- batch_7_without_using_zfill_add_zero_to_beginning_of_string.py
+++ batch_7_without_using_zfill_add_zero_to_beginning_of_string.py
@@ -1,22 +1,14 @@
 # zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
-# FUNCTION custom_zfill(text, width):
-# zeros_needed = width - LENGTH(text)
-# IF zeros_needed > 0:
-# RETURN ('0' * zeros_needed) + text
-# ELSE:
-# RETURN text
-# END FUNCTION
-# PROGRAM:
-# DISPLAY "Enter a string:"
-# GET text
-# DISPLAY "Enter total width:"
-# GET width
-# result = CALL custom_zfill(text, width)
-# DISPLAY "Result:", result
-
 def custom_zfill(text, width):
     zeros_needed = width - len(text)
     if zeros_needed > 0:
         return '0' * zeros_needed + text
     return text
+
+text = input("Enter a string: ")
+width = int(input("Enter total width: "))
+
+result = custom_zfill(text, width)
+
+print(f"Result: '{result}'")
