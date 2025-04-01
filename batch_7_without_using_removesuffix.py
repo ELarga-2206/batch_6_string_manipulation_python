df9f12c4008dfd06f80457edf4b7f66f32f1278d
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 13:34:27 +0800

    used 'text.endswith(suffix)' to check if the text string terminates with the sequence of characters in the suffix string. 'text[:-len(suffix)]' for string slicing

--- batch_7_without_using_removesuffix.py
+++ batch_7_without_using_removesuffix.py
@@ -1,1 +1,6 @@
 # removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
+def custom_removesuffix(text, suffix):
+    if text.endswith(suffix):
+        return text[:-len(suffix)]
+    return text
