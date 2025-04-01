60d82a50ccc1ee1b771a2f439de94138540ab798
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 15:49:36 +0800

    initial display of planned function, pseudocode

--- batch_7_without_using_rstrip.py
+++ batch_7_without_using_rstrip.py
@@ -1,1 +1,8 @@
 # rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# FUNCTION custom_rstrip(text):
+# end = LENGTH(text)
+# WHILE end > 0 AND text[end-1] == ' ':
+# end = end - 1
+# RETURN text[FROM START TO end]
+# END FUNCTION
