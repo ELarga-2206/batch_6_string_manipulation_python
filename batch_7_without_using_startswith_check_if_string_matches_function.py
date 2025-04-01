16bd1b94a3b0b5765dfcc550827d5cf2350d6f1e
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 16:09:37 +0800

    initial pseudocode for the function

--- batch_7_without_using_startswith_check_if_string_matches_function.py
+++ batch_7_without_using_startswith_check_if_string_matches_function.py
@@ -1,1 +1,8 @@
 # startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# FUNCTION custom_startswith(text, prefix):
+# prefix_length = LENGTH(prefix)
+# IF prefix_length == 0:
+# RETURN True
+# RETURN text[FIRST prefix_length CHARACTERS] == prefix
+# END FUNCTION
