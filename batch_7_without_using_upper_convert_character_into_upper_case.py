01a8a092ba77bcb1777e0c24236a619c51e1195f
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 16:35:05 +0800

    added user input, convertion call for uppercase text then display of result

--- batch_7_without_using_upper_convert_character_into_upper_case.py
+++ batch_7_without_using_upper_convert_character_into_upper_case.py
@@ -1,16 +1,5 @@
 # upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
-# FUNCTION custom_upper(text):
-# result = EMPTY_STRING
-# FOR EACH character IN text:
-# IF character is lowercase letter (a-z):
-#  Convert to uppercase using ASCII
-# uppercase_char = CHARACTER(ASCII(character) - 32)
-# ADD uppercase_char TO result
-# ELSE:
-# ADD character TO result  // Keep non-lowercase chars unchanged
-# RETURN result
-
 def custom_upper(text):
     result = []
     for char in text:
@@ -19,3 +8,9 @@ def custom_upper(text):
         else:
             result.append(char)
     return ''.join(result)
+
+user_input = input("Enter a string: ")
+
+uppercase_text = custom_upper(user_input)
+
+print("Uppercase:", uppercase_text)
