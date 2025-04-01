6554dfc906d5d99be0c8d2c4c60f8db59b0c7d08
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 13:44:49 +0800

    added initial pseudocode

--- batch_7_without_using_rindex_return_function_parameter_starting_from_last_character.py
+++ batch_7_without_using_rindex_return_function_parameter_starting_from_last_character.py
@@ -1,1 +1,9 @@
 # rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+#FUNCTION custom_rindex(text, substring):
+# sub_length = LENGTH(substring)
+# text_length = LENGTH(text)
+#  Start from the end and move left
+# FOR i FROM (text_length - sub_length) DOWN TO 0:
+# IF text[i TO i+sub_length] == substring:
+# RETURN i --- Last match found
