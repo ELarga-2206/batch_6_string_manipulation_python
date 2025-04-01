948f2e12917430ee54bfa825662d64764d0c33c0
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 14:28:32 +0800

    initial pseudocode for the function

--- batch_7_without_using_rjust_add_space_at_beginning.py
+++ batch_7_without_using_rjust_add_space_at_beginning.py
@@ -1,1 +1,9 @@
 #rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+#FUNCTION custom_rjust(text, width, fill_char=' '):
+# padding = width - LENGTH(text)
+# IF padding > 0:
+# RETURN (fill_char * padding) + text
+# ELSE:
+# RETURN text
+# END FUNCTION
