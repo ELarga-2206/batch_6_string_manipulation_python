f2dd2440932a295d74c4e577b34e92d51013a19d
Elarga-2206 <earl.larga22@gmail.com>
2025-04-01 12:33:38 +0800

    initial pseudo code and guide

--- batch_7_without_using_index_return_first_location_of_function.py
+++ batch_7_without_using_index_return_first_location_of_function.py
@@ -1,1 +1,14 @@
 # index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+#FUNCTION custom_index(text, substring):
+# sub_length = LENGTH(substring)
+# text_length = LENGTH(text)
+# 
+# IF sub_length == 0:
+# RETURN 0
+# 
+# FOR i FROM 0 TO (text_length - sub_length):
+# Check if current slice matches substring
+# IF text[i TO i+sub_length] == substring:
+# RETURN i ---First match found
+
